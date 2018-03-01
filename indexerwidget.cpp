84a7096e3d755eebde48f9d11b35b705407116cb
Mishko <mishko.litvin@gmail.com>
2018-03-01 15:45:04 +0200

    Bug fixed

--- indexerwidget.cpp
+++ indexerwidget.cpp
@@ -7,10 +7,8 @@ IndexerWidget::IndexerWidget(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    qDebug()<<"bubu";
     setStyleSheet(QString(("QToolButton:pressed {background-color:qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1, stop: 0 #0070FF, stop: 0.8 #3050A0,stop: 1.0 #103070)};"
                            "QToolButton:!enabled {background-color:qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1, stop: 0 #304070, stop: 0.8 #203060,stop: 1.0 #101020)};")));
-    qDebug()<<"mumu";
 
     pButtonSets = new QPushButton(this);
 
